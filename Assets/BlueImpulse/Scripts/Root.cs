c75e61f8fc9e85a5e1d2dc2742929984e128475c
i-saint <saint.skr@gmail.com>
2015-02-22 03:50:54 +0900

    rc

--- Assets/BlueImpulse/Scripts/Root.cs
+++ Assets/BlueImpulse/Scripts/Root.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Root : MonoBehaviour
 {
+    public bool m_wait_key_to_start = false;
+    public AudioSource m_audio_source;
+    public Animator m_animator;
 
     public void Quit()
     {
@@ -12,5 +15,33 @@ public class Root : MonoBehaviour
 #else
         Application.Quit();
 #endif
+    }
+
+    public void Play()
+    {
+        m_animator.enabled = true;
+        m_audio_source.enabled = true;
+        m_animator.Play("", 0, 0.0f);
+        m_audio_source.Play();
+    }
+
+    void Awake()
+    {
+        if(m_wait_key_to_start)
+        {
+            m_animator.enabled = false;
+            m_audio_source.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)) {
+            Quit();
+        }
+        if (m_wait_key_to_start && Input.GetKeyUp(KeyCode.Space))
+        {
+            Play();
+        }
     }
 }
